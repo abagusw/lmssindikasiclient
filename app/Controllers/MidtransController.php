12b021cdb3917009ffbc26128ed7dfb1265b453f
devandaandresmg <devandaandresmg@gmail.com>
2025-07-19 22:19:54 +0700

    update

--- app/Controllers/MidtransController.php
+++ app/Controllers/MidtransController.php
@@ -72,7 +72,10 @@ class MidtransController extends BaseController
         \Midtrans\Config::$isProduction = false;
 
 
-        $notif = new Notification();
+        $notifs = new Notification();
+
+        $notif = preg_replace('/[\x00-\x1F\x7F\xA0\x{200B}]/u', '', $notifs);
+
 
         $transaction_status = $notif->transaction_status;
         $payment_type       = $notif->payment_type;
@@ -100,4 +103,99 @@ class MidtransController extends BaseController
         // Respon ke Midtrans WAJIB 200 OK
         return $this->response->setStatusCode(200)->setJSON(['message' => 'Notification received']);
     }
+
+//     public function notificationX(){
+//         \Midtrans\Config::$serverKey = Midtrans_ServerKey;
+//         \Midtrans\Config::$isProduction = false;
+
+
+// // $notif = '{
+// //   "va_numbers": [
+// //     {
+// //       "va_number": "34467616648177109189528",
+// //       "bank": "bca"
+// //     }
+// //   ],
+// //   "transaction_time": "2025-07-19 21:54:49",
+// //   "transaction_status": "pending",
+// //   "transaction_id": "439a3cca-0d37-46d5-a554-481d61b49376",
+// //   "status_message": "midtrans payment notification",
+// //   "status_code": "201",
+// //   "signature_key": "97c5ae4df5f66281d68b15b40f31e97be53ff7bc8fffa40ad59af8a4f33dd512bdd5184570fe14c8496bd575ad59abec28c7c23522e3715c01434bf08a0a1992",
+// //   "payment_type": "bank_transfer",
+// //   "payment_amounts": [],
+// //   "order_id": "687bb1b750e2b",
+// //   "merchant_id": "G574834467",
+// //   "gross_amount": "75000.00",
+// //   "fraud_status": "accept",
+// //   "expiry_time": "2025-07-20 21:54:49",
+// //   "currency": "IDR"
+// // }';
+
+// $notif = '{
+//   "va_numbers": [
+//     {
+//       "va_number": "34467616648177109189528",
+//       "bank": "bca"
+//     }
+//   ],
+//   "transaction_time": "2025-07-19 21:54:49",
+//   "transaction_status": "pending",
+//   "transaction_id": "439a3cca-0d37-46d5-a554-481d61b49376",
+//   "status_message": "midtrans payment notification",
+//   "status_code": "201",
+//   "signature_key": "97c5ae4df5f66281d68b15b40f31e97be53ff7bc8fffa40ad59af8a4f33dd512bdd5184570fe14c8496bd575ad59abec28c7c23522e3715c01434bf08a0a1992",
+//   "payment_type": "bank_transfer",
+//   "payment_amounts": [],
+//   "order_id": "687bb1b750e2b",
+//   "merchant_id": "G574834467",
+//   "gross_amount": "75000.00",
+//   "fraud_status": "accept",
+//   "expiry_time": "2025-07-20 21:54:49",
+//   "currency": "IDR"
+// }';
+
+// $cleaned_notif = preg_replace('/[\x00-\x1F\x7F\xA0\x{200B}]/u', '', $notif);
+
+
+//         $dataNotif = json_decode($cleaned_notif);
+
+//         if (is_null($dataNotif)) {
+//     echo "Gagal decode JSON. Error: " . json_last_error_msg();
+//     exit;
+// }
+
+
+//         print_r('Midtrans Notif: ' . $dataNotif->transaction_status);
+//         die;
+
+//         $transaction_status = $notif->transaction_status;
+//         $payment_type       = $notif->payment_type;
+//         $order_id           = $notif->order_id;
+//         $fraud_status       = $notif->fraud_status;
+
+//         log_message('info', 'Midtrans Notif: ' . json_encode($notif));
+
+//         $paymentCallbackModel = new PaymentCallBackModel();
+//         $paymentModel = new PaymentModel();
+//         $paymentCallbackModel->updateStatusByOrderId($order_id, $transaction_status);
+
+//         $cekCallBack = $paymentCallbackModel->where('order_id', $request->order_id)->first();
+
+//         $memberModel = new MemberModel();
+
+//         $dataMember = [
+//                 'flag_active' => 1,
+//                 'isregisteredpaid' => 1
+//         ];
+
+//         $updateMember = $memberModel->update($cekCallBack->user_id,$dataMember);
+
+
+//         // Respon ke Midtrans WAJIB 200 OK
+//         return $this->response->setStatusCode(200)->setJSON(['message' => 'Notification received']);
+
+//     }
+
+
 }
