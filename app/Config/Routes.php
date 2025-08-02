56ccc2415fc279148eeae7aaf9b0301af861a0b7
devandaandresmg <devandaandresmg@gmail.com>
2025-08-02 14:08:16 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -147,7 +147,7 @@ $routes->group('/', ['filter' => 'auth'], function ($routes) {
 
 
 	//fe 	course
-	$routes->get('course/list','course::index');
+	$routes->get('course/list','Course::index');
 
 
 });
