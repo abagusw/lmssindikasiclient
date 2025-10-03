d9f72bd93461ef1a45fefd99af8abc3fcdc7f2c4
abagusw <bagusandreas13@gmail.com>
2025-10-03 18:16:49 +0800

    Update iuran awal 2 bulan

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -55,7 +55,7 @@ class Dashboard extends BaseController
             $IdbuttonPay = "failed-button";
         }
 
-        $order_id = uniqid()."|".$this->session->get('id')."|3|0";
+        $order_id = uniqid()."|".$this->session->get('id')."|2|0";
         $param = [
             'transaction_details' => [
                 'order_id' => $order_id,
@@ -69,8 +69,8 @@ class Dashboard extends BaseController
                 [
                     'id' => 'iuran_2_bulan', // ID untuk item, bisa disesuaikan
                     'name' => 'Iuran Awal 2 Bulan', // Nama item yang dibayar
-                    'price' => 50000, // Harga untuk 3 bulan
-                    'quantity' => 1, // Jumlah item (hanya 1 untuk 3 bulan)
+                    'price' => 50000, // Harga untuk 2 bulan
+                    'quantity' => 1, // Jumlah item (hanya 1 untuk 2 bulan)
                 ],
             ],
         ];
@@ -102,7 +102,7 @@ class Dashboard extends BaseController
             $params = [
                 'transaction_details' => [
                     'order_id' => $order_id,
-                    'gross_amount' => 75000,
+                    'gross_amount' => 50000,
                 ],
                 'customer_details' => [
                     'first_name' => session()->get('nama_lengkap'),
@@ -111,10 +111,10 @@ class Dashboard extends BaseController
                 ],
                 'item_details' => [
                     [
-                        'id' => 'iuran_3_bulan', // ID untuk item, bisa disesuaikan
-                        'name' => 'Iuran Awal 3 Bulan', // Nama item yang dibayar
-                        'price' => 75000, // Harga untuk 3 bulan
-                        'quantity' => 1, // Jumlah item (hanya 1 untuk 3 bulan)
+                        'id' => 'iuran_2_bulan', // ID untuk item, bisa disesuaikan
+                        'name' => 'Iuran Awal 2 Bulan', // Nama item yang dibayar
+                        'price' => 50000, // Harga untuk 2 bulan
+                        'quantity' => 1, // Jumlah item (hanya 1 untuk 2 bulan)
                     ],
                 ],
             ];
